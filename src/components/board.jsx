5310be36c0b29b0861037cb8e5520f8969ea674c
Matthew Schupack <schu34@gmail.com>
2018-03-11 18:52:33 -0400

    basic board rendering structure

--- src/components/board.jsx
+++ src/components/board.jsx
@@ -1,18 +1,29 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { Row, Col } from "react-bootstrap";
+import BoardCell from "./BoardCell";
 
+class Board extends Component {
+	render() {
+		// const boardHeight = this.props.data.length;
+		// const boardWidth = this.props.data[0].length;
 
-
-class Board extends Component{
-
-
-  render(){
-    return (
-      <div>
-        
-      </div>
-    )
-  }
+		return (
+			<div>
+				{this.props.data.map(row => {
+					return (
+						<Row>
+							{row.map(cell => {
+								<Col>
+									<BoardCell contents={cell} />
+								</Col>;
+							})}
+						</Row>
+					);
+				})}
+			</div>
+		);
+	}
 }
-
 
 export default Board;
