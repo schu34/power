477a674ca4005b141bf287d147feaaaf8add7d83
Matthew Schupack <schu34@gmail.com>
2018-06-09 18:21:23 -0400

    better coloring

--- src/components/BoardCell.jsx
+++ src/components/BoardCell.jsx
@@ -7,21 +7,33 @@ class BoardCell extends Component {
     let style = {
       backgroundColor: getColor(contents),
       opacity: getOpacity(contents)
-       
     }
+    console.log(style, contents);
     return (
-      <div className = "cell">
+      <div style={style} className="cell">
         {contents.toFixed(2)}
-        <div style={style} className='cell-color'></div>
       </div>
     )
   }
 }
 
+const maxHeat = 100;
+const minHeat = -100
 function getColor(num){
-  return num > 0 ? "red": "blue";
+  if(num === 0) return "white"
+  if(num > 0){
+    return `hsl(${scaleNum(num, maxHeat, 50, 0)},100%,${scaleNum(num, maxHeat,92, 50)}%)`
+  } else {
+    return `hsl(${scaleNum(num, minHeat, 200, 240)}, 100%, ${scaleNum(num, minHeat, 100, 50)}%`
+  }
+}
+
+function scaleNum(num, max, lower, upper ){
+  const range = upper - lower;
+  return lower + (num/max*range)
 }
 function getOpacity(num){
-  return Math.min(Math.abs(num / 100), 1);
+  return 1;
+  // return Math.min(Math.abs(num / 100), 1);
 }
 export default BoardCell
